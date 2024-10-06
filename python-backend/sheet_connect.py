5c7fa8b67fb153f78c42215a076eb52258ade2ed
tingchannavong <thipphaphone.channavong@gmail.com>
2024-10-06 14:27:54 +0700

    Added gitignore files, game master data json structure and code to fetch API find missing games round 2

--- python-backend/sheet_connect.py
+++ python-backend/sheet_connect.py
@@ -10,28 +10,32 @@ sheet = sheet_obj['Game']
 
 # Get array of image url list from column K 
 description = sheet.getColumn('K')
+print(description)
 
 #Set Image Folder
 image_folder = r"C:\Users\Macbook pro\Desktop\AWsite\python-backend\bgg_images"
 
 # Loop through the description col K, check if blank then fetch information from value of B,
-for i, game in enumerate(games, start=3):  # Start from row 3 (to skip the header and data type rows)
-    print(f"Fetching info for {game}...")
+for i, game in enumerate(description, start=3):  # Start from row 3 (to skip the header and data type rows)
+    print(f'We are in row {i}')
+    if game != '':
+        print(f"{game} info already exists...")
+        pass
+    elif game == '':
+        # Fetch game info using the BGG API
+        game_info = get_board_game_info(sheet[f'B{i}'], image_folder)
 
-    # Fetch game info using the BGG API
-    game_info = get_board_game_info(game, image_folder)
-
-    if game_info:
-        # Specify the column letters (A to H) for each attribute
-        sheet[f'G{i}'] = game_info.get('Image URL', '')  # Column G: Image URL local path, second arguement refers to what happens if not found
-        sheet[f'K{i}'] = game_info.get('Description', 'No description available') 
-        sheet[f'M{i}'] = game_info.get('Categories', 'Not specified') 
-        sheet[f'O{i}'] = game_info.get('Playing Time', 'Not specified')  
-        sheet[f'P{i}'] = game_info.get('Minimum Players', 'Not specified')  
-        sheet[f'Q{i}'] = game_info.get('Maximum Players', 'Not specified') 
-        sheet[f'R{i}'] = game_info.get('Minimum Age', 'Not specified')  
-        print(f"Details for {game} written to row {i}.")
-    else:
-        print(f"Failed to fetch details for {game}.")
+        if game_info:
+            # Specify the column letters (A to H) for each attribute
+            sheet[f'G{i}'] = game_info.get('Image URL', '')  # Column G: Image URL local path, second arguement refers to what happens if not found
+            sheet[f'K{i}'] = game_info.get('Description', 'No description available') 
+            sheet[f'M{i}'] = game_info.get('Categories', 'Not specified') 
+            sheet[f'O{i}'] = game_info.get('Playing Time', 'Not specified')  
+            sheet[f'P{i}'] = game_info.get('Minimum Players', 'Not specified')  
+            sheet[f'Q{i}'] = game_info.get('Maximum Players', 'Not specified') 
+            sheet[f'R{i}'] = game_info.get('Minimum Age', 'Not specified')  
+            print(f"Details for {game} written to row {i}.")
+        else:
+            print(f"Failed to fetch details for {game}.")
 
 print("Finished writing all game details.")
