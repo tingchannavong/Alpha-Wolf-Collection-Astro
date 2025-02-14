eed4e15868a958adaa31868d6c28720a10b4dcc8
tingchannavong <thipphaphone.channavong@gmail.com>
2025-02-14 21:54:04 +0700

    -fixed display of playing time and min players

--- src/scripts/search.ts
+++ src/scripts/search.ts
@@ -80,8 +80,8 @@ function displaySearchResults(results): void {
                 </p>
                 <p class="game-category">Category: ${game.frontmatter.category}</p>
                <div class="game-info-container">
-                <p class="game-location">Playing Time:  ${game.frontmatter.location} Minutes</p>
-                <p class="game-location">Minimum Players:  ${game.frontmatter.location}</p>
+                <p class="game-location">Playing Time:  ${game.frontmatter.playing_time} Minutes</p>
+                <p class="game-location">Minimum Players:  ${game.frontmatter.min_players}</p>
                 <p class="game-location">Maximum Players:  ${game.frontmatter.max_players}</p>
                 <p class="game-location">Location: ${game.frontmatter.location}</p>
             </div>
